d7a3aaaf1353ded5bf3e143903cc2034984a5db3
benniezhu <bz22@nyu.edu>
2019-07-11 08:44:22 -0400

    Add AgeCat X Axis No Lag Plots and Code

--- code/misc/MakeAgeCatXAxisPlot.r
+++ code/misc/MakeAgeCatXAxisPlot.r
@@ -23,3 +23,38 @@ ggplot(data = mAllCountriesTotal, aes(x=agecat, y = value, group = year))+
   labs(title = "All Countries Total")+
   labs(x = "Age Category")+
   labs(y = "Coefficient")
+
+
+
+
+
+
+#AllCountriesFemaleNoLag
+
+ggplot(data = mAllCountriesFemaleNoLag, aes(x=agecat, y = value, group = year))+
+  geom_line(aes(color = year))+
+  geom_point(aes(color=year))+
+  labs(title = "All Countries FemaleNoLag")+
+  labs(x = "Age Category")+
+  labs(y = "Coefficient")
+
+#AllCountriesMaleNoLag
+
+ggplot(data = mAllCountriesMaleNoLag, aes(x=agecat, y = value, group = year))+
+  geom_line(aes(color = year))+
+  geom_point(aes(color=year))+
+  labs(title = "All Countries MaleNoLag")+
+  labs(x = "Age Category")+
+  labs(y = "Coefficient")
+
+#AllCountriesTotalNoLag
+ggplot(data = mAllCountriesTotalNoLag, aes(x=agecat, y = value, group = year))+
+  geom_line(aes(color = year))+
+  geom_point(aes(color=year))+
+  labs(title = "All Countries TotalNoLag")+
+  labs(x = "Age Category")+
+  labs(y = "Coefficient")
+
+
+
+
