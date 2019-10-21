24eacac82a67d220300d2528348f79550d063650
benniezhu <bz22@nyu.edu>
2019-10-21 11:02:37 -0400

    organize folder strucutre of the datasetup code folder

--- code/data_setup/Reshapedata.R
+++
@@ -1,9 +0,0 @@
-install.packages("readstata13")
-library(readstata13)
-library(tidyverse)
-allcountries <- read.dta13("C:/Users/sw1386/Downloads/AllCountries.dta")
-n<-names(allcountries)
-allc_rb<-c(allcountries [,2:length(n)])
-allc_rb_ul<-data.frame(unlist(allc_rb))
-
-write.csv(allc_rb_ul, file="C:/Users/sw1386/Downloads/allc_rb_ul.csv")
