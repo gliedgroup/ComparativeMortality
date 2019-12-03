87cd86c5b3a16682907786d81b99ff92c483c941
benniezhu <bz22@nyu.edu>
2019-12-03 13:07:09 -0500

    Add Loop example

--- code/misc/CauseOfDeathFigures/GroupedBars.r
+++ code/misc/CauseOfDeathFigures/GroupedBars.r
@@ -9,9 +9,16 @@ agecat1 =c("7(5-9)", "8(10-14)", "9(15-19)")
 
 agecat2 = c("10(20-24)", "11(25-29)", "12(30-34)", "13(35-39)", "14(40-44)", "15(45-49)", "16(50-54)", "17(55-59)", "18(60-64)", "19(65-69)", "20(70-74)", "21(75-79)", "22(80-84)")
 
+## One way of doing this in a loop for future reference
+for (var in seq_along(List)) {
+  print(
+    (ggplot(subset(ExternalCauses,agecatstr== List[var] & sex == 1 & year%%10==0), aes(fill = countrystr, y = WeightedECMortalityPer100kByAge, x = year ))+
+          geom_bar(position = "dodge", stat = "identity")+
+          ggtitle(paste(List[var]))))
+}
 
 
-## Males 
+## Males
 
 
 ggplot(subset(ExternalCauses, agecat == 7 & sex == 1 & year%%10==0), aes(fill = countrystr, y = WeightedECMortalityPer100kByAge, x = year ))+
@@ -117,7 +124,7 @@ ggplot(subset(ExternalCauses, agecat == 22 & sex == 1 & year%%10==0), aes(fill =
 
 ggsave("80-84 Male.pdf")
 
-##Females 
+##Females
 
 ggplot(subset(ExternalCauses, agecat == 7 & sex == 2 & year%%10==0), aes(fill = countrystr, y = WeightedECMortalityPer100kByAge, x = year ))+
   geom_bar(position = "dodge", stat = "identity")+
@@ -221,4 +228,3 @@ ggplot(subset(ExternalCauses, agecat == 22 & sex == 2 & year%%10==0), aes(fill =
   ggtitle("80-84 Female")
 
 ggsave("80-84 Female.pdf")
-
